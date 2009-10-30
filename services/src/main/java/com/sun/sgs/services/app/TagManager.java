a17545dc487c0932e44decb58b73002120baaf83
stp <stp@8bb07e50-ac56-45bb-ed21-9da535a92e52>
2009-10-30 16:45:56 +0000

    Added initial support for a new service that allows applications to "tag" tasks. Reports associated with these tagged tasks are streamed to listeners in the same style as profiling uses. The ext.properties file was updated to include this service, though it shuts down if no listeners are provided so by default nothing new is running.
    git-svn-id: https://svn.java.net/svn/darkstar-services~svn/trunk@13 8bb07e50-ac56-45bb-ed21-9da535a92e52

---
+++ services/src/main/java/com/sun/sgs/services/app/TagManager.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2009 Sun Microsystems, Inc.
+ *
+ * This file is part of Project Darkstar Server.
+ *
+ * Project Darkstar Server is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation and
+ * distributed hereunder to you.
+ *
+ * Project Darkstar Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sun.sgs.services.app;
+
+
+/**
+ * A {@code Manager} that provides application-level functionality similar
+ * to profile reporting. An application developer may use this interface to
+ * report various details that will be associated with the current transaction.
+ * The resulting collections of details are reported as a stream to
+ * listener objects on the local node.
+ * <p>
+ * Note that this is still experimental, with several open questions. For
+ * instance, would a registration system like the one in the profiling
+ * interfaces be useful? You'd get a handle that would allow for typing,
+ * or other possible meta-data but that would require extra state and
+ * data store access with each report. Samples or aggregations might be
+ * possible, but anything detailed will be very hard to provide in a
+ * cluster-wide sense.
+ * <p>
+ * Currently the only value that can be reported is a "tag", which is just
+ * some numeric identifier. This allows application code to tag a given
+ * transaction with multiple identifiers that capture something specific
+ * about the application logic (i.e. game-specific events). A tag may
+ * optionally be applied with some un-typed value to capture some state
+ * associated with the event. For now, a given tag may only be used once
+ * in a given transaction.
+ */
+public interface TagManager {
+
+    /**
+     * Tags the current task with the given identifier.
+     *
+     * @param tag the tag identifier
+     *
+     * @return {@code true} if the tag was applied, {@code false} if the
+     *         identifier has already been used in this task
+     */
+    boolean tagTask(long tag);
+
+    /**
+     * Tags the current task with the given identifier, associating the
+     * given value with this tag. The value may be any arbitrary object,
+     * but will later be accessed in a non-transactional state and so
+     * should not be a reference into the data store.
+     *
+     * @param tag the tag identifier
+     * @param tagValue opaque value for the tag
+     *
+     * @return {@code true} if the tag was applied, {@code false} if the
+     *         identifier has already been used in this task
+     */
+    boolean tagTask(long tag, Object tagValue);
+
+}
---
+++ services/src/main/java/com/sun/sgs/services/impl/service/TagReportListener.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2009 Sun Microsystems, Inc.
+ *
+ * This file is part of Project Darkstar Server.
+ *
+ * Project Darkstar Server is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation and
+ * distributed hereunder to you.
+ *
+ * Project Darkstar Server is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.sun.sgs.services.impl.service;
+
+/**
+ * The interface used to listen for {@code TagReport}s as they are generated.
+ * As with the interface used to listen to {@code ProfileReport}s, all
+ * reports are sequential meaning that implementations do not need to
+ * handle multiple reports in parallel. Listeners should do the minimum
+ * work required to process any given report so that they do not block
+ * other listeners or the progress of the stream of all reports.
+ * <p>
+ * All implementations of {@code TagReportListener} must have a constructor
+ * with two parameters: {@code Properties} and {@code long}. The first
+ * parameter provides access to the properties used to start the local node
+ * and the second parameter is the local node's unique identifier.
+ */
+public interface TagReportListener {
+
+    /**
+     * Notifies the listener that a tagged task has completed and provides
+     * the complete report associated with that task.
+     *
+     * @param tagReport the report for the tagged task
+     */
+    void report(TagReport tagReport);
+
+    /**
+     * Tells the listener to stop working, most likely because the local node
+     * is being shutdown. Implementations should use this as a chance to
+     * free up any resources being held like open files or network
+     * connections. Note that in cases of node failure, this method may
+     * not be called, so implementors should not assume that they will always
+     * have a chance to perform this graceful shutdown.
+     */
+    void shutdown();
+
+}
